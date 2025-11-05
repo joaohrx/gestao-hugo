eded836a5263d61d6111f541a604be20d35c0b2e
edu-code249 <eduardo.santos2@escolar.ifrn.edu.br>
2025-11-05 14:05:22 -0300

    nova
    ertfwefg

--- database_trabalho.sql
+++ database_trabalho.sql
@@ -4,8 +4,3 @@ use gestao_departamentos_2v;
 ## tb_cargos (car_id, car_nome, car_salario_base)
 ## tb_funcionarios (fun_id, fun_nome, fun_cargo_id, fun_dep_id, fun_data_admissao)
 
-create table tb_departamentos (
-dep_id INT AUTO_INCREMENT PRIMARY KEY,
-dep_nome varchar(50)NOT NULL,	
-dep_gerente_id INT NOT NULL
-);	
