3b20820767c58bed72834859175b7f0a2e9745ca
Mdrs-Arthur <medeiros.arthur1@escolar.ifrn.edu.br>
2025-11-05 14:07:56 -0300

    Merge branch 'main' of https://github.com/joaohrx/gestao-hugo

--- database_trabalho.sql
+++ database_trabalho.sql
@@ -1,6 +1,24 @@
 create database gestao_departamentos_2v;
 use gestao_departamentos_2v;
-##- tb_departamentos (dep_id, dep_nome, dep_gerente_id)
-## tb_cargos (car_id, car_nome, car_salario_base)
-## tb_funcionarios (fun_id, fun_nome, fun_cargo_id, fun_dep_id, fun_data_admissao)
 
+create table tb_departamentos(
+dep_id int auto_increment primary key,
+dep_nome varchar(100) not null,
+dep_gerente_id int not null
+);
+
+create table tb_cargos(
+car_id int auto_increment primary key,
+car_nome varchar(100) not null,
+car_salario_base decimal not null
+);
+
+create table tb_funcionarios(
+fun_id int auto_increment primary key,
+fun_nome varchar(100) not null,
+fun_car_id int not null,
+fun_dep_id int not null,
+fun_data_admissao date not null,
+foreign key (fun_car_id) references tb_cargos(car_id),
+foreign key (fun_dep_id) references tb_departamentos(dep_id)
+);
