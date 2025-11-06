cc55198af4af72b8647f58415ef1d4498d5df160
joaohrx <victor.rocha1@escolar.ifrn.edu.br>
2025-11-06 17:35:46 -0300

    atualizacao

--- database_trabalho.sql
+++ database_trabalho.sql
@@ -1,10 +1,11 @@
 create database gestao_departamentos_2v;
 use gestao_departamentos_2v;
+drop table gestao_departamentos_2v;
 
 create table tb_departamentos(
 dep_id int auto_increment primary key,
 dep_nome varchar(100) not null,
-dep_gerente_id int not null
+dep_gerente_id int
 );
 
 create table tb_cargos(
@@ -25,25 +26,25 @@ foreign key (fun_dep_id) references tb_departamentos(dep_id)
 
 insert into tb_departamentos(dep_nome, dep_gerente_id) values
 ('Finanças', '1'),
-('Comercial','3'),
-('Logística', '8'),
-('Comercial', '9'),
-('Administrativo','11')
+('Comercial','2'),
+('Logística',  NULL),
+('Comercial', NULL),
+('Administrativo', '3')
 ;
 
 insert into tb_cargos (car_nome, car_salario_base) values
 ('Assistente', 2500),
-('Gerente', 6000),
+('Gerente', 6500),
 ('Consultor', 4500),
 ('Analista', 5000),
 ('Diretor', 12000)
 ;
 
 insert into tb_funcionarios (fun_nome, fun_car_id, fun_dep_id, fun_data_admissao) values
-('Joaildo Campos', '2', '11', '2021-11-10'),
-('Vinícius Maia', '2', '9', '2024-05-06'),
-('Maria Camila', '1', '8', '2025-06-30'),
-('Kafka da Silva', '3', '1', '2023-03-15'),
-('Rodolfo Pires', '4', '3', '2024-06-20'),
-('Luciano Lamarte', '5', '11', '2021-07-15')
+('Joaildo Campos', 2, 1, '2021-11-10'),
+('Vinícius Maia', 2, 2, '2024-05-06'),
+('Maria Camila', 1, 3, '2025-06-30'),
+('Kafka da Silva', 3, 4, '2023-03-15'),
+('Rodolfo Pires', 4, 3, '2024-06-20'),
+('Luciano Lamarte', 5, 2, '2021-07-15')
 ;
